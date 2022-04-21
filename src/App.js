67beefcae55bb085f10fa849694c02cf2077d2a9
Hamza Maqsood <hamzamaqsood080@gmail.com>
2022-04-21 20:12:37 +0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Profile from "./components/Profile";
 import Data from "./components/Data";
 function App() {
+  console.log("hello");
   return (
     <div className="App">
       <Data />
